0dcc617a63f9c490e809b3043320edcb04e9a35b
simonesanna1983 <35135396+simonesanna1983@users.noreply.github.com>
2023-09-29 15:14:41 +0200

    2 files modified

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,34 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 import { App } from './App';
 
-// const root = createRoot(document.getElementById('app'));
 const container = document.querySelector('#shadow-root');
-
 const shadowContainer = container.attachShadow({ mode: 'open' });
-
+const emotionRoot = document.createElement('style');
 const shadowRootElement = document.createElement('div');
-
+shadowContainer.appendChild(emotionRoot);
 shadowContainer.appendChild(shadowRootElement);
+
+const cache = createCache({
+  key: 'css',
+  prepend: true,
+  container: emotionRoot,
+});
+
 ReactDOM.createRoot(shadowRootElement).render(
   <React.StrictMode>
-    {/* <CacheProvider value={cache}> */}
-    <div>Shadow DOM</div>
+    <CacheProvider value={cache}>
+      <div>Inside Shadow DOM</div>
+    </CacheProvider>
     <App />
-    {/* </CacheProvider> */}
+  </React.StrictMode>
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
   </React.StrictMode>
 );
